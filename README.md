19d213f13731334baf6af9a4f3a0903beb84f00d
- <gitkraken@mail.decouplr.com>
2018-11-07 15:28:54 +0100

    Introduce parcel.js as builder

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# hiss
+# HISS
 
 High Intensity Super Slow App
+
+## Setup
+
+1. Install nodejs
+2. Install yarn
+3. Run `yarn`
+
+## Usage
+
+- `yarn serve` to start the development server
+- `yarn build` to build the app
---
+++ src/assets/js/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  #app {
+    text-align: center;
+  }
+</style>

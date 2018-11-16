ab3319c44b9b4ee64c7035aa63e61cc32868047e
- <gitkraken@mail.decouplr.com>
2018-11-16 19:54:55 +0100

    Add different exercise types and templates

--- src/assets/js/store/modules/exercises/state.js
+++ src/assets/js/store/modules/exercises/state.js
@@ -1,11 +1,21 @@
 export default {
     exercises: [
-        { id: '1', name: 'Push-ups' },
-        { id: '2', name: 'Pull-ups' },
-        { id: '3', name: 'Wall-sits' },
-        { id: '4', name: 'Squats' },
-        { id: '5', name: 'Bar-dips' },
-        { id: '6', name: 'Lying Pull-ups' },
-        { id: '7', name: 'Planks' },
+        { id: '1',  type: 'machine',     name: 'Seated Row' },
+        { id: '2',  type: 'machine',     name: 'Chest Press' },
+        { id: '3',  type: 'machine',     name: 'Pull Down' },
+        { id: '4',  type: 'machine',     name: 'Overhead Press' },
+        { id: '5',  type: 'machine',     name: 'Leg Press' },
+        { id: '6',  type: 'free-weight', name: 'Bent-Over Barbell Row' },
+        { id: '7',  type: 'free-weight', name: 'Standing Overhead Press' },
+        { id: '8',  type: 'free-weight', name: 'Dead Lift' },
+        { id: '9',  type: 'free-weight', name: 'Bench Press' },
+        { id: '10', type: 'free-weight', name: 'Squat' },
+        { id: '11', type: 'bodyweight',  name: 'Push-ups' },
+        { id: '12', type: 'bodyweight',  name: 'Pull-ups' },
+        { id: '13', type: 'bodyweight',  name: 'Wall-sits' },
+        { id: '14', type: 'bodyweight',  name: 'Squats' },
+        { id: '15', type: 'bodyweight',  name: 'Bar-dips' },
+        { id: '16', type: 'bodyweight',  name: 'Lying Pull-ups' },
+        { id: '17', type: 'bodyweight',  name: 'Planks' },
     ]
 }
--- src/assets/js/store/modules/templates/state.js
+++ src/assets/js/store/modules/templates/state.js
@@ -1,7 +1,7 @@
 export default {
     templates: [
-        { id: '1', name: 'Big Five',   exercises: ['1', '2', '3', '4', '5']},
-        { id: '2', name: 'Big Three',  exercises: ['1', '2', '3']},
-        { id: '3', name: 'Bodyweight', exercises: ['3', '4', '5', '6', '7']},
+        { id: '1', name: 'Big Five',              exercises: ['1', '2', '3', '4', '5']},
+        { id: '2', name: 'Big Five Free-Weights', exercises: ['6', '7', '8', '9', '10']},
+        { id: '3', name: 'Big Seven Bodyweight',  exercises: ['11', '12', '13', '14', '15', '16', '17']},
     ]
 }

486ba35511b72e167903472c75b2e76897e99cf9
- <gitkraken@mail.decouplr.com>
2018-11-19 12:31:34 +0100

    Show template name instead of template id

--- src/assets/js/components/Template.vue
+++ src/assets/js/components/Template.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h2>Template {{ id }}</h2>
+        <h2>{{ template.name }}</h2>
         <ExerciseList
             v-bind:exercises = exercises>
         </ExerciseList>

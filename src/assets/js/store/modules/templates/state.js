b6b3991615a0e731dad67a9dbc309b1923ca3ac8
- <gitkraken@mail.decouplr.com>
2018-11-19 12:31:57 +0100

    Add Big Three workout templates

--- src/assets/js/store/modules/templates/state.js
+++ src/assets/js/store/modules/templates/state.js
@@ -1,7 +1,9 @@
 export default {
     templates: [
-        { id: '1', name: 'Big Five',              exercises: ['1', '2', '3', '4', '5']},
-        { id: '2', name: 'Big Five Free-Weights', exercises: ['6', '7', '8', '9', '10']},
-        { id: '3', name: 'Big Seven Bodyweight',  exercises: ['11', '12', '13', '14', '15', '16', '17']},
+        { id: '1', name: 'Big Five',               exercises: ['1', '2', '3', '4', '5']},
+        { id: '2', name: 'Big Five Free-Weights',  exercises: ['6', '7', '8', '9', '10']},
+        { id: '3', name: 'Big Seven Bodyweight',   exercises: ['11', '12', '13', '14', '15', '16', '17']},
+        { id: '4', name: 'Big Three',              exercises: ['2', '3', '5']},
+        { id: '5', name: 'Big Three Free-Weights', exercises: ['6', '9', '10']},
     ]
 }

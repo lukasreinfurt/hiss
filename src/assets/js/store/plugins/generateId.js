0a3b89806195b952841c9b1ba537fbf108938384
- <gitkraken@mail.decouplr.com>
2018-12-18 07:56:45 +0100

    Pad id sub strings to constant lenghts

--- src/assets/js/store/plugins/generateId.js
+++ src/assets/js/store/plugins/generateId.js
@@ -2,14 +2,28 @@ const generateId = function(store) {
   store.generateId = function() {
     const date = new Date();
     const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    const milliseconds = date.getMilliseconds();
-    const id =
-      "" + year + month + day + hours + minutes + seconds + milliseconds;
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date
+      .getDate()
+      .toString()
+      .padStart(2, "0");
+    const hours = date
+      .getHours()
+      .toString()
+      .padStart(2, "0");
+    const minutes = date
+      .getMinutes()
+      .toString()
+      .padStart(2, "0");
+    const seconds = date
+      .getSeconds()
+      .toString()
+      .padStart(2, "0");
+    const milliseconds = date
+      .getMilliseconds()
+      .toString()
+      .padStart(3, "0");
+    const id = year + month + day + hours + minutes + seconds + milliseconds;
     return id;
   };
 };

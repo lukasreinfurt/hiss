e3729d79d1357f738778fc0861eddf6b68748141
- <gitkraken@mail.decouplr.com>
2018-12-27 13:45:53 +0100

    Moved top navigation to side bar with responsive hamburger menu

--- src/assets/js/components/App.vue
+++ src/assets/js/components/App.vue
@@ -1,19 +1,57 @@
 <template>
-  <div id="app">
-    <nav>
-      <ul class="flex-container horizontal">
-        <li><router-link to="/log">Log</router-link></li>
-        <li><router-link to="/exercises">Exercises</router-link></li>
-        <li><router-link to="/workouts">Workouts</router-link></li>
-        <li><router-link to="/version">Version</router-link></li>
+  <div id="app" class="flex-container horizontal">
+    <nav id="main-nav" :class="{ active: menuActive }" @click="deactivateMenu">
+      <ul class="flex-container">
+        <li>
+          <router-link to="/log" @click.native="deactivateMenu"
+            >Log</router-link
+          >
+        </li>
+        <li>
+          <router-link to="/exercises" @click.native="deactivateMenu"
+            >Exercises</router-link
+          >
+        </li>
+        <li>
+          <router-link to="/workouts" @click.native="deactivateMenu"
+            >Workouts</router-link
+          >
+        </li>
+        <li>
+          <router-link to="/version" @click.native="deactivateMenu"
+            >Version</router-link
+          >
+        </li>
       </ul>
     </nav>
-    <router-view />
+    <div id="mainWrapper" class="flex-container">
+      <nav id="secondary-nav">
+        <ul class="flex-container horizontal">
+          <li id="hamburger-menu" @click="toggleMenu">☰</li>
+        </ul>
+      </nav>
+      <div id="contentWrapper" class="flex-container">
+        <div id="content"><router-view /></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  data: function() {
+    return {
+      menuActive: false
+    };
+  },
+  methods: {
+    toggleMenu: function() {
+      this.menuActive = !this.menuActive;
+    },
+    deactivateMenu: function() {
+      this.menuActive = false;
+    }
+  }
 };
 </script>

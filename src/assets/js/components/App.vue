d657c8f390b9df2bfe949db90d690fae44c1cb6b
- <gitkraken@mail.decouplr.com>
2019-01-01 21:18:17 +0100

    Fix main navigation links being too small

--- src/assets/js/components/App.vue
+++ src/assets/js/components/App.vue
@@ -3,10 +3,10 @@
     <aside class="flex-container">
       <nav id="main-nav" :class="{ active: menuActive }" @click="toggleMenu">
         <ul class="flex-container">
-          <li><router-link to="/log">Log</router-link></li>
-          <li><router-link to="/exercises">Exercises</router-link></li>
-          <li><router-link to="/workouts">Workouts</router-link></li>
-          <li><router-link to="/version">Version</router-link></li>
+          <router-link to="/log"><li>Log</li></router-link>
+          <router-link to="/exercises"><li>Exercises</li></router-link>
+          <router-link to="/workouts"><li>Workouts</li></router-link>
+          <router-link to="/version"><li>Version</li></router-link>
         </ul>
       </nav>
     </aside>

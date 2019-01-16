f295d438443681318833e0624007fd943fe68b36
- <gitkraken@mail.decouplr.com>
2019-01-16 20:41:15 +0100

    Add BaseLayout component

---
+++ src/assets/js/components/BaseLayout.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="baseLayout flex-container">
+		<header class="baseLayout__header"><slot name="header"></slot></header>
+		<main class="baseLayout__main"><slot name="main"></slot></main>
+		<footer class="baseLayout__footer"><slot name="footer"></slot></footer>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "BaseLayout"
+};
+</script>

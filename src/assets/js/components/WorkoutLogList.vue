8ae4f7b0e7ac2400cf2012dcc810c64fa4f97849
- <gitkraken@mail.decouplr.com>
2019-01-17 20:53:28 +0100

    Add BaseLayout and ParallaxContainer to WorkoutLogList

--- src/assets/js/components/WorkoutLogList.vue
+++ src/assets/js/components/WorkoutLogList.vue
@@ -1,5 +1,57 @@
 <template>
-	<div id="contentWrapper" class="flex-container">
+	<BaseLayout class="workoutLogList">
+		<template slot="header">
+			<NavBar :title="title" menu-button>
+				<template slot="right">
+					<button @click="addNewWorkoutLog()">＋</button>
+				</template>
+			</NavBar>
+		</template>
+		<template slot="main">
+			<ParallaxContainer>
+				<template slot="deep">
+					<div class="workoutLogList__headerContainer headerContainer">
+						<img
+							class="workoutLogList__illustration"
+							src="/assets/images/illustrations/log.png"
+						/>
+					</div>
+				</template>
+				<template slot="back">
+					<div class="workoutLogList__headerContainer headerContainer">
+						<h1 class="workoutLogList__title">{{ title }}</h1>
+					</div>
+				</template>
+				<template slot="front">
+					<div class="workoutLogList__contentWrapper contentWrapper">
+						<div
+							v-if="count > 0"
+							class="workoutLogList__contentContainer contentContainer"
+						>
+							<WorkoutLogListItem
+								v-for="workoutLog in workoutLogs"
+								:key="workoutLog.id"
+								:workout-log="workoutLog"
+							></WorkoutLogListItem>
+						</div>
+						<div
+							v-else
+							class="workoutLogList__contentContainer contentContainer -isEmpty"
+						>
+							<p>
+								Seems like you haven't logged any workouts yet. <br />Go on, add
+								your first log now!
+							</p>
+							<button class="primary" @click="addNewWorkoutLog()">
+								Add Log
+							</button>
+						</div>
+					</div>
+				</template>
+			</ParallaxContainer>
+		</template>
+	</BaseLayout>
+	<!-- <div id="contentWrapper" class="flex-container">
 		<header>
 			<NavBar :title="title" menu-button>
 				<template slot="right">
@@ -27,21 +79,27 @@
 				</div>
 			</main>
 		</div>
-	</div>
+  </div>-->
 </template>
 
 <script>
+import BaseLayout from "./BaseLayout";
 import NavBar from "./NavBar";
+import ParallaxContainer from "./ParallaxContainer";
 import WorkoutLogListItem from "./WorkoutLogListItem";
 import { createNamespacedHelpers } from "vuex";
 
-const { mapState, mapActions } = createNamespacedHelpers("workoutLogs");
+const { mapState, mapGetters, mapActions } = createNamespacedHelpers(
+	"workoutLogs"
+);
 
 export default {
 	name: "WorkoutLogList",
 	components: {
-		WorkoutLogListItem,
-		NavBar
+		BaseLayout,
+		NavBar,
+		ParallaxContainer,
+		WorkoutLogListItem
 	},
 	data: function() {
 		return {
@@ -49,6 +107,7 @@ export default {
 		};
 	},
 	computed: {
+		...mapGetters(["count"]),
 		...mapState(["workoutLogs"])
 	},
 	methods: {
